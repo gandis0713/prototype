cf1c696d096ca670d4c710079727cc6bcaf15a6d
gandis <gandis0713@gmail.com>
2021-09-11 14:53:43 +0900

    fix: delete webgl prototype

--- opengl/webgl/webpack.config.js
+++
@@ -1,9 +0,0 @@
-const spath = require('path');
-
-module.exports = {
-    entry: './src/index.js',
-    output: {
-        filename: 'main.js',
-        path: spath.resolve(__dirname, "dist"),
-    },
-}
