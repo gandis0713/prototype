666c2eb1616acea49bcdc2ea9f4d8b0b1ada9813
gandis <gandis0713@gmail.com>
2022-01-30 16:57:23 +0900

    feat: test set

--- language/cpp/set_test/main.cpp
+++ language/cpp/set_test/main.cpp
@@ -1,38 +1,61 @@
 #include <iostream>
 #include <set>
 #include <string>
+// #include <hash>
+#include <unordered_set>
 
 
 using namespace std;
 
+class ACmp;
 class A
 {
   int value;
   std::string name;
 public:
-  bool operator<(const A& rhs) const 
+  // bool operator<(const A& rhs) const 
+  // {
+  //   return this->value < rhs.value;
+  // }
+  bool operator==(const A& rhs) const 
   {
-    return this->value < rhs.value;
+    std::cout << "test1 " << std::endl;
+    return this->value == rhs.value;
+  }
+
+  friend class std::hash<A>;
+};
+
+namespace std
+{
+
+template<>
+class hash<A>
+{
+public:
+  size_t operator()(const A& rhs) const
+  {
+    std::hash<std::string> hash_func;
+    std::cout << "test " << std::endl;
+    return rhs.value ^ hash_func(rhs.name);
   }
 };
 
-class ACmp
-{
-public:
-  bool operator()(const A& lhs, const A& rhs) const
-  {
-    std::cout << "test " << std::endl;
-    return false;
-  }
-};
+}
+
 
 
 int main() 
 {
-  std::set<A, ACmp> data;
+  // std::set<A, ACmp> data;
+  std::unordered_set<A> data_unrodered;
 
   A a;
-  data.insert(a);
+  A a1;
+  // data.insert(a);
+
+  data_unrodered.insert(a);
+  data_unrodered.insert(a1);
 
   return 0;
 }
