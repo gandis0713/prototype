24b9800dbfccc8486af1306deb283316bf34562f
gandis <gandis0713@gmail.com>
2022-01-31 14:07:55 +0900

    feat: test r value

---
+++ language/cpp/r_value/main.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <string>
+
+class CString
+{
+public:
+
+  CString()
+  {
+    std::cout << "CString()" << std::endl;
+  }
+
+  CString(const char* c)
+  {
+    std::cout << "CString() : " << c << std::endl;
+    str = c;
+  }
+
+  CString(const CString& rhs)
+  {
+    std::cout << "CString(const CString& rhs) : " << rhs.str << std::endl;
+    this->str = rhs.str;
+  }
+
+  CString(CString&& rhs) noexcept
+  {
+    std::cout << "CString(CString&& rhs) : " << rhs.str << std::endl;
+    this->str = rhs.str;
+  }
+
+  ~CString() 
+  {
+    std::cout << "~CString" << std::endl;
+  }
+
+  CString operator+(const CString& rhs)
+  {
+    std::string str;
+    str = this->str + rhs.str;
+
+    CString cstr;
+    cstr.setString(str);
+
+    return cstr;
+  }
+
+  void setString(const std::string& str)
+  {
+    this->str = str;
+  }
+
+private:
+  std::string str;
+};
+
+int main()
+{
+  CString str1 = "abc";
+  CString str2 = "def";
+
+  CString str3 = str1 + str2;
+  CString str4(static_cast<CString&&>(str3));
+  CString str5(str4);
+
+  return 0;
+}
