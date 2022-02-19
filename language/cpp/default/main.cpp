7c19a5c9f70af4d04b7e58b2677916ce563573f4
gandis <gandis0713@gmail.com>
2022-02-19 13:50:31 +0900

    feat: test default keywork

--- language/cpp/default/main.cpp
+++ language/cpp/default/main.cpp
@@ -5,15 +5,28 @@ class A
 public:
   A() = default;
   A(const A& rhs) = default;
-  A(int v) {};
+  A& operator=(const A& rhs) = default;
+  // A& operator=(const A& rhs)
+  // {
+  //   std::cout << "operator copy" << std::endl;
+  //   return *this;
+  // }
+
+  int v;
 };
+
+
 
 int main()
 {
-  std::cout << "default" << std::endl;
 
   A a1;
   A a2 = a1;
+  a2.v = 10;
+  A a3;
+  a3 = a2;
+  std::cout << "a3 : " << a3.v << std::endl;
+  std::cout << "a2 : " << a2.v << std::endl;
 
   return 0;
 }
